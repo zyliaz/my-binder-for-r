4379e6184a6693a5df9df410cd39aed3bdad9c31
zyliaz <zyliduck4758@gmail.com>
2025-09-17 00:36:38 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,6 @@
 install.packages("readr")
+install.packages("tidyverse", dependencies = TRUE)
+install.packages("rmarkdown", dependencies = TRUE)
+install.packages("IRkernel", dependencies = TRUE)
+
+IRkernel::installspec(user = FALSE)
